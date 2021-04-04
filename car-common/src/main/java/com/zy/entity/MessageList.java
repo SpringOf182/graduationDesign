830e4d9b948ffffea74f56c974b1b23bc1eee0ab
Ying <zy@openingsource.org>
2021-04-04 18:53:17 +0800

    添加messageList注释

--- car-common/src/main/java/com/zy/entity/MessageList.java
+++ car-common/src/main/java/com/zy/entity/MessageList.java
@@ -10,15 +10,33 @@ import java.util.Date;
 @Data
 public class MessageList {
 
+    /**
+     * 消息发送者ID.
+     */
     private String senderUID;
 
+    /**
+     * 消息接收者ID.
+     */
     private String receiverUID;
 
+    /**
+     * 最新一条消息发送时间.
+     */
     private Date time;
 
+    /**
+     * 最新一条消息内容.
+     */
     private String message;
 
+    /**
+     * 对方用户头像url.
+     */
     private String image;
 
+    /**
+     * 对方用户昵称.
+     */
     private String nickName;
 }
